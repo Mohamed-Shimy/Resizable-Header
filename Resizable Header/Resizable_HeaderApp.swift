a53108dad8b3eac654e1529d7128bc642e49550d
Mohamed Shimy <msh.work@outlook.sa>
2023-05-03 22:06:20 +0300

    Initial Commit

---
+++ Resizable Header/Resizable_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resizable_HeaderApp.swift
+//  Resizable Header
+//
+//  Created by Mohamed Shemy on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Resizable_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
